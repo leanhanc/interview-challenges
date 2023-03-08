5b1a936c66b170c714db04cae7d672041a0398a7
Leandro Hanc <leandrohanc@gmail.com>
2023-03-08 13:21:22 -0300

    String-termina-con: solution

--- algorithms-4/string-termina-con/index.ts
+++ algorithms-4/string-termina-con/index.ts
@@ -2,6 +2,6 @@ export default function stringTerminaCon(
   string: string,
   final: string
 ): boolean {
-  // TODO: implement
-  return false;
+  // Easy solution
+  return string.endsWith(final);
 }
