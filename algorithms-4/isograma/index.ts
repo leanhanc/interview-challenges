e3b623ca0a99a24f04ddfb33fd1bc25c26288072
Leandro Hanc <leandrohanc@gmail.com>
2023-03-08 12:54:58 -0300

    Isograma: solution and fixed tests

--- algorithms-4/isograma/index.ts
+++ algorithms-4/isograma/index.ts
@@ -1,4 +1,13 @@
 export default function esIsograma(string: string): boolean {
-  // TODO: implement
-  return false;
+  const normalized = string
+    .toLocaleLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .split("");
+  const set = new Set(normalized);
+
+  const original = normalized.join("");
+  const nonDuplicates = Array.from(set).join("");
+
+  return original === nonDuplicates;
 }
