c10fb742fd035564330234c44378e7411df0f911
Leandro Hanc <leandrohanc@gmail.com>
2023-03-08 12:45:16 -0300

    Elementos-pares solution and fixed test.

--- algorithms-4/elementos-pares/index.ts
+++ algorithms-4/elementos-pares/index.ts
@@ -1,4 +1,19 @@
 export default function filtrarPares(array: unknown[]): unknown[] {
-  // TODO: implement
-  return [];
+  let counter: Record<string, number> = {};
+  if (!Array.isArray(array)) return [];
+  for (const element of array) {
+    if (counter[element] > 0) {
+      counter[element] = counter[element] + 1;
+    } else {
+      counter[element] = 1;
+    }
+  }
+
+  for (const key in counter) {
+    if (counter[key] % 2 !== 0) {
+      delete counter[key];
+    }
+  }
+
+  return Object.keys(counter).map((t) => Number(t));
 }
