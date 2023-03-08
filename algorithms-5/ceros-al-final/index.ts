27de93fe1b1d55798cad233b5f04fc41f5c90280
Leandro Hanc <leandrohanc@gmail.com>
2023-03-08 17:24:28 -0300

    moverCeros: solution

--- algorithms-5/ceros-al-final/index.ts
+++ algorithms-5/ceros-al-final/index.ts
@@ -1,4 +1,15 @@
 export default function moverCeros(array: unknown[]): unknown[] {
-  // TODO: implement
-  return [];
+  if (typeof array !== "object" || !Array.isArray(array)) {
+    return [];
+  }
+  const originalLength = array.length;
+  const noZeros = array.filter((item) => item !== 0);
+  const zerosToAdd = originalLength - noZeros.length;
+
+  const out = [...noZeros];
+  for (let index = 0; index < zerosToAdd; index++) {
+    out.push(0);
+  }
+
+  return out;
 }
