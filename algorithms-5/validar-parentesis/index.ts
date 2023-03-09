5256c1d1fe9c467ee6bffa5b40136909da40e32a
Leandro Hanc <leandrohanc@gmail.com>
2023-03-09 00:59:04 -0300

    Validar-parentesis: solution

--- algorithms-5/validar-parentesis/index.ts
+++ algorithms-5/validar-parentesis/index.ts
@@ -1,4 +1,14 @@
-export default function validarParéntesis(parentesis: string): boolean {
-  // TODO: implement
-  return false;
+export default function validarParéntesis(cadena: string): boolean {
+  const stack: string[] = [];
+  for (let i = 0; i < cadena.length; i++) {
+    if (cadena[i] === "(") {
+      stack.push(cadena[i]);
+    } else if (cadena[i] === ")") {
+      if (stack.length === 0) {
+        return false;
+      }
+      stack.pop();
+    }
+  }
+  return stack.length === 0;
 }
